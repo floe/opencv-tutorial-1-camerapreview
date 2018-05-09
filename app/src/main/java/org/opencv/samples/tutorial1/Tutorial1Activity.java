cf256b58a6b5d572da79e7ec967c645ae26c6657
Florian Echtler <floe@butterbrot.org>
2018-05-09 23:31:16 +0200

    face tracking example

--- app/src/main/java/org/opencv/samples/tutorial1/Tutorial1Activity.java
+++ app/src/main/java/org/opencv/samples/tutorial1/Tutorial1Activity.java
@@ -7,8 +7,16 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -17,12 +25,19 @@ import android.view.SurfaceView;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Tutorial1Activity extends Activity implements CvCameraViewListener2 {
     private static final String TAG = "OCVSample::Activity";
 
     private CameraBridgeViewBase mOpenCvCameraView;
     private boolean              mIsJavaCamera = true;
     private MenuItem             mItemSwitchCamera = null;
+    private CascadeClassifier    cc = null;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -32,6 +47,7 @@ public class Tutorial1Activity extends Activity implements CvCameraViewListener2
                 {
                     Log.i(TAG, "OpenCV loaded successfully");
                     mOpenCvCameraView.enableView();
+                    cc = new CascadeClassifier(initAssetFile("haarcascade_frontalface_default.xml"));
                 } break;
                 default:
                 {
@@ -57,8 +73,23 @@ public class Tutorial1Activity extends Activity implements CvCameraViewListener2
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
 
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_FRONT);
 
         mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    // as per https://developer.android.com/reference/android/content/Context#getExternalFilesDir()
+    // asset files need to be put into app/src/main/assets/
+    public String initAssetFile(String filename)  {
+        File file = new File(getFilesDir(), filename);
+        if (!file.exists()) try {
+            InputStream is = getAssets().open(filename);
+            OutputStream os = new FileOutputStream(file);
+            byte[] data = new byte[is.available()];
+            is.read(data); os.write(data); is.close(); os.close();
+        } catch (IOException e) { e.printStackTrace(); }
+        Log.d(TAG,"prepared local file: "+filename);
+        return file.getAbsolutePath();
     }
 
     @Override
@@ -95,6 +126,15 @@ public class Tutorial1Activity extends Activity implements CvCameraViewListener2
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+        //if (cc == null) cc = new CascadeClassifier(initLocalFile("haarcascade_frontalface_default.xml"));
+        MatOfRect results = new MatOfRect();
+        Mat gray = inputFrame.gray();
+        Mat col  = inputFrame.rgba();
+        cc.detectMultiScale(gray,results);
+
+        for (Rect foo: results.toArray())
+            Imgproc.rectangle(col, foo.tl(), foo.br(), new Scalar(0, 0, 255), 3);
+
+        return col;
     }
 }
